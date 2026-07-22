bb6409e5b83df98e5c7493457d81a2feb34fdc05
usmanovart <96758545+usmanovart@users.noreply.github.com>
2026-07-22 10:10:37 +0500

    initial commit

---
+++ apps/web/app/(user)/profile/page.tsx
@@ -0,0 +1,9 @@
+import { ProfilePageContent } from "@/components/profile/ProfilePageContent";
+
+export default function ProfilePage() {
+  return (
+    <div className="p-6 md:p-8">
+      <ProfilePageContent />
+    </div>
+  );
+}
